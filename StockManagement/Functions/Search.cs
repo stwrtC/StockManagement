15ce9fb0367f02ac89f192a78b1645ec92a02bab
Connor Stewart <Connor.Stewart@tpr.gov.uk>
2023-09-11 15:37:18 +0100

    Changed GetIdsByName to generic

---
+++ StockManagement/Functions/Search.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    public class Search
+    {
+        public List<int?> GetIdsByName<T>(List<T> stock, string? name) where T: Stock
+        {
+            if (String.IsNullOrEmpty(name) == false)
+            {
+                List<int?> items = stock.Where(x => x.Name == name).Select(x => x.Id).ToList();
+                return items;
+            }
+            return null;
+        }
+    }
+}

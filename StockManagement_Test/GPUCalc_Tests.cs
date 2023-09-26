e25c1ff5bef5d4b4aaa667d97474667ba93a4a5d
Connor Stewart <121879496+m3tamorphosis@users.noreply.github.com>
2023-09-26 12:21:39 +0100

    Moq Tests for Calculator class

---
+++ StockManagement_Test/GPUCalc_Tests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using StockManagement;
+using StockManagement.Services;
+
+namespace StockManagement_Test
+{
+    public class GPUCalc_Tests
+    {
+        [Test]
+        public void TotalStockTest()
+        {
+            // Arrange
+            var mockGPURepo = new Mock<GPURepository>();
+            var gpuCalc = new GPUCalc(mockGPURepo.Object);
+            // Act
+            int result = gpuCalc.TotalStock();
+            // Assert
+            Assert.That(result, Is.EqualTo(6));
+        }
+        [Test]
+        public void TotalValueTest()
+        {
+            // Arrange
+            var mockGPURepo = new Mock<GPURepository>();
+            var gpuCalc = new GPUCalc(mockGPURepo.Object);
+            // Act
+            var result = gpuCalc.TotalValue();
+            // Assert
+            Assert.That(result, Is.EqualTo(1909.98m));
+        }
+
+
+    }
+}
---
+++ StockManagement_Test/LaptopCalc_Tests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using StockManagement.Services;
+using StockManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement_Test
+{
+    internal class LaptopCalc_Tests
+    {
+        [Test]
+        public void TotalStockTest()
+        {
+            // Arrange
+            var mockLaptopRepo = new Mock<LaptopRepository>();
+            var laptopCalc = new LaptopCalc(mockLaptopRepo.Object);
+            // Act
+            int result = laptopCalc.TotalStock();
+            // Assert
+            Assert.That(result, Is.EqualTo(5));
+        }
+        [Test]
+        public void TotalValueTest()
+        {
+            // Arrange
+            var mockLaptopRepo = new Mock<LaptopRepository>();
+            var laptopCalc = new LaptopCalc(mockLaptopRepo.Object);
+            // Act
+            var result = laptopCalc.TotalValue();
+            // Assert
+            Assert.That(result, Is.EqualTo(199));
+        }
+    }
+}
